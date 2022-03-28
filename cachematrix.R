03ee3aa7c3a3d19fc6135ba4794fab19c35c2365
RomanoManuel <87090887+RomanoManuel@users.noreply.github.com>
2022-03-28 23:19:49 +0200

    Final version ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,25 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+  k <- NULL ##set variable null
+  set <- function(y){
+  x <<- y ## Assign a value to an object in an environment that is different from the current environment
+  k <<- NULL
+  }
+  get <- function()x
+  setInverse <- function(inverse) k <<- inverse ##set inverse in the cache
+  getInverse <- function() k
+  list(set = set, get = get, 
+  setInverse = setInverse, 
+  getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  k <- x$getInverse()
+  if(!is.null(k)){
+  message("cache data") 
+  return(k) ## Return the inverse of x
+  }
+  mat <- x$get()
+  k <- solve(mat,...)
+  x$setInverse(k)
+  j
 }
